c4dbaaecf5e6598939d97989d14119c7ee725984
sergiolstavares <sergio.leo.tavares@gmail.com>
2025-01-27 10:50:06 -0300

    FIX - cleaning code

--- src/app/components/task/components/delete-task-modal/delete-task-modal.component.ts
+++ src/app/components/task/components/delete-task-modal/delete-task-modal.component.ts
@@ -1,16 +1,17 @@
 import { Component, Inject } from '@angular/core'
-import { MatButtonModule } from '@angular/material/button'
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 import { SharedMaterialModule } from '../../../shared-material.module'
 
 @Component({
   selector: 'app-delete-task-modal',
+  standalone: true,
   templateUrl: './delete-task-modal.component.html',
   styleUrls: ['./delete-task-modal.component.css'],
   imports: [SharedMaterialModule]
 })
 export class DeleteTaskModalComponent {
   taskTitle = ''
+
   constructor(
     public dialogRef: MatDialogRef<DeleteTaskModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { id: string, title: string }
